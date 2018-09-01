887535da9f04d9ea7102ba0730c88313df5e1f59
shun <hmhmsh0122@gmail.com>
2018-09-01 13:30:38 +0900

    Simplify

--- Sources/MewExample/Main/ValueInput/ValueInputViewController.swift
+++ Sources/MewExample/Main/ValueInput/ValueInputViewController.swift
@@ -68,13 +68,14 @@ final class ValueInputViewController: UIViewController, Instantiatable, Interact
     }
 
     @IBAction func buttonTapped(_ sender: Any) {
+        let output = Output.init(inputedValue: Int(textField.text ?? "0") ?? 0)
         switch model.kind {
         case .push:
             navigationController?.popViewController(animated: true)
-            handler?(Output.init(inputedValue: Int(textField.text ?? "0") ?? 0))
+            handler?(output)
         case .present:
             dismiss(animated: true, completion: {
-                self.handler?(Output.init(inputedValue: Int(self.textField.text ?? "0") ?? 0))
+                self.handler?(output)
             })
         }
     }
